4adbb20189ca640315e4f66f157b916765f00297
Shubham Rawat <codingblocks@Codings-MacBook-Pro.local>
2020-03-13 00:27:32 +0530

    lec

---
+++ src/com/company/Lec20/BinaryClient.java
@@ -0,0 +1,24 @@
+package com.company.Lec20;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BinaryClient {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file =new File("input");
+        Scanner s = new Scanner(file);
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(s);
+        tree.insert(s);
+        tree.insert(s);
+        tree.insert(s);
+
+        tree.postorder();
+        System.out.println(tree.height());
+    }
+}
+

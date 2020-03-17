c50fc8be16ea00df1e39414f0c22a274882fc9c5
ShubhamRwt <shubhamrwt02@gmail.com>
2020-03-17 13:48:28 +0530

    lec

--- src/com/company/Lec22/BSTclient.java
+++ src/com/company/Lec22/BSTclient.java
@@ -7,11 +7,17 @@ public class BSTclient {
 
 
        BalancedBST tree = new BalancedBST();
+
+        tree.insert(1);
+        tree.insert(4);
+        tree.insert(3);
+
+        tree.preorder();
+
+        System.out.println(tree.isbalanced());
+//        tree.insert(4);
 //        tree.insert(5);
-//        tree.insert(2);
-//        tree.insert(7);
-//        tree.insert(3);
-//        tree.insert(1);
+
 
 //        System.out.println(tree.successor(2));
 //
--- src/com/company/Lec22/BalancedBST.java
+++ src/com/company/Lec22/BalancedBST.java
@@ -25,11 +25,79 @@ public class BalancedBST {
 
         node.height= Math.max(height(node.left),height(node.right))+1;
 
+        node=balancefactor(node);
         return node;
     }
 
+    private Node balancefactor(Node node) {
+
+        if(height(node.left)-height(node.right)>1){
+
+            if(height(node.left.left)-height(node.left.right)<0){
+
+                node.left=leftrotation(node.left);
+            }
+
+            return rightrotation(node);
+        }
+
+        if (height(node.left) - height(node.right) < -1) {
+
+            if(height(node.right.left)-height(node.right.right)>0){
+
+                node.right=rightrotation(node.right);
+            }
+
+            return leftrotation(node);
+        }
+
+        return node;
+    }
+
+    public void preorder(){
+        preorder(root,"");
+    }
+
+    private void preorder(Node node, String indent){
+
+        if(node==null){
+            return;
+        }
+
+        System.out.println(indent+node.value);
+
+        preorder(node.left,indent+"\t");
+        preorder(node.right,indent+"\t");
+
+    }
 
 
+    private Node leftrotation(Node y){
+
+        Node x =y.right;
+        Node temp= x.left;
+
+        x.left=y;
+        y.right=temp;
+
+        y.height=Math.max(height(y.left),height(y.right))+1;
+        x.height=Math.max(height(x.left),height(x.right))+1;
+
+        return x;
+    }
+
+    private Node rightrotation(Node x){
+        Node y= x.left;
+        Node temp=y.right;
+
+        y.right=x;
+        x.left=temp;
+
+        x.height=Math.max(height(x.left),height(x.right))+1;
+        y.height=Math.max(height(y.left),height(y.right))+1;
+
+        return y;
+    }
 
     private int height(Node node){
 
@@ -38,6 +106,28 @@ public class BalancedBST {
         }
 
         return node.height;
+    }
+
+    public boolean isbalanced(){
+        return isbalanced(this.root);
+    }
+
+    private boolean isbalanced(Node node) {
+
+        if(node==null){
+            return true;
+        }
+
+        int diff= Math.abs(height(node.left)-height(node.right));
+
+        if(diff>1){
+            return false;
+        }
+
+        boolean left=isbalanced(node.left);
+        boolean right=isbalanced(node.right);
+
+        return left && right;
     }
 
 
